47117b58066f36eadcf5d1a1362373de001bb5c5
Taneesha <taneesha.r166@stu.nebo.edu>
2018-09-20 10:28:25 -0600

    Added "score manager"  and "death box" script

---
+++ New Unity Project/Assets/Scripts/DeathBox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour {
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.name == "PC")
+        {
+            Debug.Log("PC Enters Death Zone");
+            Destroy(other);
+        }
+    }
+}
