2472a65f5c84837ecae8bfb63ef945a5631d137a
Taneesha <taneesha.r166@stu.nebo.edu>
2018-10-10 10:29:17 -0600

    Added player shoot, projectile

---
+++ New Unity Project/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public Transform FirePoint;
+    public GameObject Projectile;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightControl))
+            Instantiate(Projectile, FirePoint.position, FirePoint.rotation);
+	}
+}
---
+++ New Unity Project/Assets/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float Speed;
+    public Rigidbody2D PC;
+    public GameObject EnemyDeath;
+    public GameObject ProjectileParticle;
+    public int PointsForKill;
+
+	// Use this for initialization
+	void Start () {
+        // PC = FindObjectOfType<Rigidbody2D>();
+        if (PC.transform.localScale.x < 0)
+            Speed = -Speed;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PC.transform.localScale.x < 0)
+            //Speed = -Speed;
+            GetComponent<Rigidbody2D>
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Enemy"){
+            Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+            ScoreManager.AddPoints(PointsForKill);
+        }
+
+        Instantiate(ProjectileParticle, transform.position, transform.rotation);
+        Destroy(gameObject);
+
+        }
+	}
